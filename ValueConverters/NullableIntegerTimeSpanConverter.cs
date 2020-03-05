3cb01c92c0ef6e745ecbb63d4a0aa9222279a0d3
Dean Lunz <deanlunz@createdbyx.com>
2020-03-05 15:02:56 -0800

    added additioal code files (some are not used) referenced xceed toolkit for the integer up down control

---
+++ ValueConverters/NullableIntegerTimeSpanConverter.cs
@@ -0,0 +1,103 @@
+﻿namespace AutoDownloader.ValueConverters
+{
+    using System;
+
+    public enum TimeSpanValue
+    {
+        Hours,
+        Minutes,
+        Seconds
+    }
+
+
+
+    /// <summary>
+    /// ValueConvertor class that converts a nullable integer to a string and back.
+    /// </summary>
+    /// <remarks>
+    /// Useful for databinding string value properties to nullable integer properties.
+    /// </remarks>
+    [System.Windows.Data.ValueConversion(typeof(TimeSpan), typeof(int?))]
+    public class NullableIntegerTimeSpanConverter : System.Windows.Data.IValueConverter
+    {
+        public TimeSpanValue Part
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Converts a nullable integer to a string.
+        /// </summary>
+        /// <param name="value">The value produced by the binding source</param>
+        /// <param name="targetType">The type of the binding target property</param>
+        /// <param name="parameter">The converter parameter to use</param>
+        /// <param name="culture">The culture to use in the converter</param>
+        /// <returns>Converted value</returns>
+        public object Convert(object value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value != null && !(value is int?))
+            {
+                throw new System.InvalidOperationException("The source must be a nullable integer");
+            }
+
+            if (targetType != typeof(TimeSpan))
+            {
+                throw new System.InvalidOperationException("The target must be a TimeSpan");
+            }
+
+            var intValue = (int?)value;
+            if (intValue.HasValue)
+            {
+                switch (this.Part)
+                {
+                    case TimeSpanValue.Hours:
+                        return TimeSpan.FromHours(intValue.Value);
+
+                    case TimeSpanValue.Minutes:
+                        return TimeSpan.FromMinutes(intValue.Value);
+
+                    case TimeSpanValue.Seconds:
+                        return TimeSpan.FromSeconds(intValue.Value);
+                }
+            }
+
+            return TimeSpan.Zero;
+        }
+
+        /// <summary>
+        /// Converts a string to a nullable integer.
+        /// </summary>
+        /// <param name="value">The value produced by the binding source</param>
+        /// <param name="targetType">The type of the binding target property</param>
+        /// <param name="parameter">The converter parameter to use</param>
+        /// <param name="culture">The culture to use in the converter</param>
+        /// <returns>Converted value</returns>
+        public object ConvertBack(object value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (!(value is TimeSpan))
+            {
+                throw new System.InvalidOperationException("The source must be a TimeSpan");
+            }
+
+            if (targetType != typeof(int?))
+            {
+                throw new System.InvalidOperationException("The target must be a nullable integer");
+            }
+
+            var timeValue = (TimeSpan) value;
+            switch (this.Part)
+            {
+                case TimeSpanValue.Hours:
+                    return timeValue.Hours;
+
+                case TimeSpanValue.Minutes:
+                    return timeValue.Minutes;
+
+                case TimeSpanValue.Seconds:
+                    return timeValue.Seconds;
+            }
+
+            return default(int?);
+        }
+    }
+}
---
+++ ViewModels/TimeSpanControlViewModel.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections;
+using Codefarts.AppCore;
+
+namespace AutoDownloader.ViewModels
+{
+    using System.Windows.Input;
+    using Codefarts.AutoDownloader;
+    using Codefarts.WPFCommon.Commands;
+
+    public class TimeSpanControlViewModel : PropertyChangedBase
+    {
+        private PluginEntryModel modelReference;
+
+        public int Seconds
+        {
+            get
+            {
+                var model = this.modelReference;
+                return model != null ? model.Interval.Seconds : 0;
+            }
+
+            set
+            {
+                var model = this.modelReference;
+                if (model == null)
+                {
+                    return;
+                }
+
+                var currentValue = model.Interval.Seconds;
+                if (currentValue != value)
+                {
+                    var interval = this.modelReference.Interval;
+                    model.Interval = new TimeSpan(interval.Hours, interval.Minutes, value);
+                }
+            }
+        }
+
+        public int Minutes
+        {
+            get
+            {
+                var model = this.modelReference;
+                return model != null ? model.Interval.Minutes : 0;
+            }
+
+            set
+            {
+                var model = this.modelReference;
+                if (model == null)
+                {
+                    return;
+                }
+
+                var currentValue = model.Interval.Minutes;
+                if (currentValue != value)
+                {
+                    var interval = this.modelReference.Interval;
+                    model.Interval = new TimeSpan(interval.Hours, value, interval.Seconds);
+                }
+            }
+        }
+
+        public int Hours
+        {
+            get
+            {
+                var model = this.modelReference;
+                return model != null ? model.Interval.Hours : 0;
+            }
+
+            set
+            {
+                var model = this.modelReference;
+                if (model == null)
+                {
+                    return;
+                }
+
+                var currentValue = model.Interval.Hours;
+                if (currentValue != value)
+                {
+                    var interval = this.modelReference.Interval;
+                    model.Interval = new TimeSpan(value, interval.Minutes, interval.Seconds);
+                }
+            }
+        }
+
+        public PluginEntryModel ModelReference
+        {
+            get
+            {
+                return this.modelReference;
+            }
+
+            set
+            {
+                var currentValue = this.modelReference;
+                if (currentValue != value)
+                {
+                    if (currentValue != null)
+                    {
+                        currentValue.PropertyChanged -= this.ModelReference_PropertyChanged;
+                    }
+
+                    this.modelReference = value;
+                    if (value != null)
+                    {
+                        value.PropertyChanged += this.ModelReference_PropertyChanged;
+                    }
+
+                    this.NotifyOfPropertyChange(() => this.ModelReference);
+                    this.NotifyOfPropertyChange(() => this.Hours);
+                    this.NotifyOfPropertyChange(() => this.Minutes);
+                    this.NotifyOfPropertyChange(() => this.Seconds);
+                }
+            }
+        }
+
+        private void ModelReference_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case "Interval":
+                    this.NotifyOfPropertyChange(() => this.Hours);
+                    this.NotifyOfPropertyChange(() => this.Minutes);
+                    this.NotifyOfPropertyChange(() => this.Seconds);
+                    break;
+            }
+        }
+
+        public ICommand SetModel
+        {
+            get
+            {
+                return new GenericDelegateCommand<PluginEntryModel>(
+                    model =>
+                    {
+                        if (model == null)
+                        {
+                            return false;
+                        }
+
+                        return true;
+                    },
+                    model => { this.ModelReference = model; });
+            }
+        }
+    }
+}
